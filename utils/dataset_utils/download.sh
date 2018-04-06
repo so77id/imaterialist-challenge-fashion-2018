aa3d11f46d0a2f54bce375c4f076a38013da018d
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2018-04-06 02:00:28 -0300

    fix download.sh

--- utils/dataset_utils/download.sh
+++ utils/dataset_utils/download.sh
@@ -1,3 +1,3 @@
-python3 -m utils.dataset_utils.dataset_downloader ~/.kaggle/competitions/imaterialist-challenge-fashion-2018/train.json ./dataset/train
-python3 -m utils.dataset_utils.dataset_downloader ~/.kaggle/competitions/imaterialist-challenge-fashion-2018/validation.json ./dataset/validation
-python3 -m utils.dataset_utils.dataset_downloader ~/.kaggle/competitions/imaterialist-challenge-fashion-2018/test.json ./dataset/test
+python3 -m utils.dataset_utils.dataset_downloader ~/.kaggle/competitions/imaterialist-challenge-fashion-2018/train.json ./dataset/kaggle/imaterialist-challenge-fashion-2018/train
+python3 -m utils.dataset_utils.dataset_downloader ~/.kaggle/competitions/imaterialist-challenge-fashion-2018/validation.json ./dataset/kaggle/imaterialist-challenge-fashion-2018/validation
+python3 -m utils.dataset_utils.dataset_downloader ~/.kaggle/competitions/imaterialist-challenge-fashion-2018/test.json ./dataset/kaggle/imaterialist-challenge-fashion-2018/test
