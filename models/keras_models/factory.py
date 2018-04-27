1d83da34e814f4bce1fde9ad378d064ce435fc34
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2018-04-27 18:22:57 -0300

    Add keras pretrained models (#9)
    * add densenet 121 from keras standar
    
    * fix batch size drom densenet 121
    
    * add input_tensor to model in densenet121
    
    * add resnet 50
    
    * dropout reshape layers
    
    * add xception network
    
    * add inception resenet v2
    
    * add densetnet 169
    
    * add densenet 201

--- models/keras_models/factory.py
+++ models/keras_models/factory.py
@@ -1,16 +1,35 @@
 from models.keras_models.inception_v4 import inception_v4_model
-from models.keras_models.resnet_50 import resnet50_model
-from models.keras_models.densenet_121 import densenet121_model
 
+# from models.keras_models.resnet_50 import resnet50_model
+from models.keras_models.resnet_50_keras import resnet50_keras_model
 
-def model_factory(model_name, img_rows, img_cols, channel, num_classes, dropout_keep_prob=0, checkpoint=""):
+# from models.keras_models.densenet_121 import densenet121_model
+from models.keras_models.densenet_121_keras import densenet121_keras_model
+from models.keras_models.densenet_169_keras import densenet169_keras_model
+from models.keras_models.densenet_201_keras import densenet201_keras_model
+
+from models.keras_models.xception import xception_model
+
+from models.keras_models.inception_resnet_v2 import inception_resnet_v2_model
+
+def model_factory(model_name, img_rows, img_cols, channels, num_classes, dropout_keep_prob=0, checkpoint="", freeze=False):
 
     if model_name == 'inception_v4':
-        model = inception_v4_model(img_rows, img_cols, channel, num_classes, dropout_keep_prob=dropout_keep_prob)
+        model = inception_v4_model(img_rows, img_cols, channels, num_classes, dropout_keep_prob=dropout_keep_prob)
     elif model_name == 'resnet_50':
-        model = resnet50_model(img_rows, img_cols, channel, num_classes)
+        # model = resnet50_model(img_rows, img_cols, channels, num_classes)
+        model = resnet50_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
     elif model_name == 'densenet_121':
-        model = densenet121_model(img_rows, img_cols, channel, num_classes=num_classes, dropout_rate=dropout_keep_prob)
+        # model = densenet121_model(img_rows, img_cols, channels, num_classes=num_classes, dropout_rate=dropout_keep_prob)
+        model = densenet121_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
+    elif model_name == 'densenet_169':
+        model = densenet169_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
+    elif model_name == 'densenet_201':
+        model = densenet201_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
+    elif model_name == 'xception':
+        model = xception_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
+    elif model_name == 'inception_resnet_v2':
+        model = inception_resnet_v2_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
 
     if checkpoint != '':
         print("Loading checkpoint:", checkpoint)
---
+++ models/keras_models/xception.py
@@ -0,0 +1,31 @@
+from keras.applications.xception import Xception
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Input
+
+
+def xception_model(img_rows=299, img_cols=299, channels=3, num_classes=1000, freeze=False, dropout_keep_prob=0.2):
+    # this could also be the output a different Keras model or layer
+    input_tensor = Input(shape=(img_rows, img_cols, channels))  # this assumes K.image_data_format() == 'channels_last'
+    # create the base pre-trained model
+    base_model = Xception(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dropout(dropout_keep_prob)(x)
+    # x = Flatten()(x)
+    x = Dense(1024, activation='relu')(x)
+
+    predictions = Dense(units=num_classes, activation='sigmoid')(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions, name='Xception')
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    if freeze:
+        for layer in base_model.layers:
+            layer.trainable = False
+
+
+    return model
