1d66c6fb0d7f5419bc01be2180de070f65a4ff73
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2018-04-27 18:21:12 -0300

    add densenet 201

--- models/keras_models/factory.py
+++ models/keras_models/factory.py
@@ -1,11 +1,12 @@
 from models.keras_models.inception_v4 import inception_v4_model
 
-from models.keras_models.resnet_50 import resnet50_model
+# from models.keras_models.resnet_50 import resnet50_model
 from models.keras_models.resnet_50_keras import resnet50_keras_model
 
-from models.keras_models.densenet_121 import densenet121_model
+# from models.keras_models.densenet_121 import densenet121_model
 from models.keras_models.densenet_121_keras import densenet121_keras_model
 from models.keras_models.densenet_169_keras import densenet169_keras_model
+from models.keras_models.densenet_201_keras import densenet201_keras_model
 
 from models.keras_models.xception import xception_model
 
@@ -23,6 +24,8 @@ def model_factory(model_name, img_rows, img_cols, channels, num_classes, dropout
         model = densenet121_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
     elif model_name == 'densenet_169':
         model = densenet169_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
+    elif model_name == 'densenet_201':
+        model = densenet201_keras_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
     elif model_name == 'xception':
         model = xception_model(img_rows, img_cols, channels, num_classes=num_classes, freeze=freeze, dropout_keep_prob=dropout_keep_prob)
     elif model_name == 'inception_resnet_v2':
